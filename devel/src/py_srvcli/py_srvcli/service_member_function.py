7163b56a9ebdd549173953be9a6b5f48eecea33a
Gabriel Reis <43526807+gabriel-rreis@users.noreply.github.com>
2021-05-08 12:05:50 -0300

    Added development files

---
+++ devel/src/py_srvcli/py_srvcli/service_member_function.py
@@ -0,0 +1,35 @@
+from example_interfaces.srv import AddTwoInts
+
+import rclpy
+from rclpy.node import Node
+
+
+class MinimalService(Node):
+
+    def __init__(self):
+        super().__init__('minimal_service')
+        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
+
+    def add_two_ints_callback(self, request, response):
+        # A little modification to test how the service responds to changes while running
+        if (request.a == 2 and request.b == 2):
+            response.sum = 5
+        else:
+            response.sum = request.a + request.b
+        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+
+        return response
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_service = MinimalService()
+
+    rclpy.spin(minimal_service)
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
